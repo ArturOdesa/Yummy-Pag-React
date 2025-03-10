42f0d7df0966c62f25b09ef1a750f7e26da5f3f3
Artur Stoianov <stoianovart@gmail.com>
2025-03-11 00:54:03 +0200

    complete Footer block with it's styles, add media

---
+++ yammy-pag-react/src/blocks/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import ('./Footer.css');
+
+function Footer() {
+    return (
+        <footer>
+        <div class="footer-container">
+            <div class="footer-content">
+                <p class="footer-text">Приєднуйся і живи зі мною у світі солодощів!</p>
+            </div>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer;
