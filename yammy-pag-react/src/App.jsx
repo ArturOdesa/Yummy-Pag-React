5b60b238dd2aaf6c1d008e5783578d73c213dbbc
Artur Stoianov <stoianovart@gmail.com>
2025-03-11 00:54:32 +0200

    add Footer block to project

--- yammy-pag-react/src/App.jsx
+++ yammy-pag-react/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./blocks/Header/Header"
 import MainBlock from "./blocks/Main/Main-block"
+import Footer from "./blocks/Footer/Footer";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       < MainBlock />
+      < Footer />
     </>
   )
 }
